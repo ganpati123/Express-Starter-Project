e618f639aaca9040693ff000ceeec25c8fa64b13
Ganpati kumar <ganpati.dbg00@gmail.com>
2024-12-17 11:42:37 +0530

    connected to mongodb server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const serverConfig = require('./config/serverConfig');
+const connentDB = require("./config/dbConfig");
 
 const app = express();
 
-app.listen(serverConfig.PORT, () => {
-    console.log(`Server started at port ${serverConfig.PORT}...!!`);
-})
+app.listen(serverConfig.PORT, async() => {
+   await connentDB();
+   console.log(`Server started at port ${serverConfig.PORT}...!!`);
+});
