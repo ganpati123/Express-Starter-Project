f02995f3679817c954b403896071447625e01f86
Ganpati kumar <ganpati.dbg00@gmail.com>
2024-12-17 17:58:33 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const serverConfig = require('./config/serverConfig');
 const connentDB = require("./config/dbConfig");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+
+
 
 app.listen(serverConfig.PORT, async() => {
    await connentDB();
